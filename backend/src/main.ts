bb7fe4deb53832e7c11bf2e21fada246ce666143
Kirill Begej <fleki679@yandex.ru>
2026-06-06 12:38:47 +0300

    config: первоначальная настройка nest

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,16 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const DEFAULT_PORT = 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('app.port') || DEFAULT_PORT;
+
+  await app.listen(port);
 }
+
 bootstrap();
